516a7be9115f390b5aadf4d2962d443b2abff11a
the-reverbcs <35312312+the-reverbcs@users.noreply.github.com>
2018-01-30 08:45:53 -0800

    Update Space_Aliens___The_Game.py

--- Space_Aliens___The_Game.py
+++ Space_Aliens___The_Game.py
@@ -28,14 +28,14 @@ def console_riddle():
     print("Please follow the following riddle access the control panel.\n")
 
     import time
-    ##time.sleep(1)
+    time.sleep(1)
 
     print("A man dies and needs to go to heaven, but an angel and devil is blocking his way. The man needs to say something to both of them at the same")
     print("time in order to reach heaven, but the devil does the opposite of what the man says and the angel does what the man asks.")
     print("What does the man need to say to both of them in order to go to heaven?") 
 
     for i in range(3, 0, -1):
-        ##time.sleep(1)
+        time.sleep(1)
         user_answer = input("\nPlease type in your answer: ")
         user_answer = user_answer.lower()
 
@@ -49,7 +49,7 @@ def console_riddle():
 
             if i - 1 == 0:
                 print("Releasing organism killing gas to remove non human objects...\n")
-                ##time.sleep(1)
+                time.sleep(1)
                 print("GAME OVER.")
 
                 import sys
@@ -60,24 +60,24 @@ def console_riddle():
 def console_access():
     print("** You touch the screen of the ship's main control console. ** ")
     import time
-    ##time.sleep(1)
+    time.sleep(1)
 
     print("WARNING. WARNING.")
     
-    ##time.sleep(2)
+    time.sleep(2)
     print("\n27 unidentified moving objects on board. Please evacuate the ship as soon as possible.")
     
-    ##time.sleep(2)
+    time.sleep(2)
     print("\n ** You go the the security camera panel. ** ")
     
-    ##time.sleep(3)
+    time.sleep(3)
     print("\nThese things are aliens!? How can things get any worse?")
     print("\n** You look at security camera live footage. ** ")
     
-    ##time.sleep(2)
+    time.sleep(2)
     print("\nOh no. There are still people on this ship!")
     
-    ##time.sleep(1)
+    time.sleep(1)
     print("What do I do? The only way I can save these people from these aliens is to hack the airlock doors so the aliens get sucked out into space!")
 
 
@@ -87,27 +87,27 @@ def user_leaves():
     print("No! I can't do anything for them at this point! It's too risky to do anything that rash at this time!")
     print("I need to get out of here as fast as possible!")
 
-    ##time.sleep(1)
+    time.sleep(1)
 
     print("** You start creeping out, into the hallway. You hear sounds of the aliens right behind you. **\n")
-    ##time.sleep(2)
+    time.sleep(2)
     print("** You keep walking as silently as possible until you see the escape pods, then you break into a dash to get to the escape pod quicker. ** \n")
-    ##time.sleep(1)
+    time.sleep(1)
     print("** You accidentally kick a piece of scrap metal on the floor, creating a really loud noise that attracts the aliens. ** \n")
-    ##time.sleep(1)
+    time.sleep(1)
     print("** You panic even more and run into one of the escape pods as the noises of aliens catch up behind you. ** ")
-    ##time.sleep(1)
+    time.sleep(1)
     print("\n** You get into the pod, and sucessfully close it. You start it up and launch off into the open space, but then you get suspicious. **")
-    ##time.sleep(1)
+    time.sleep(1)
     print("\nThis seemed way too easy. Thank god I'm alive.")
-    ##time.sleep(1)
+    time.sleep(1)
     print("\n** Something along the lines of slime drips onto your shoulder. ** ")
-    ##time.sleep(1)
+    time.sleep(1)
     print("\n** You look into the window and see a reflection. You turn around. **")
-    ##time.sleep(1)
+    time.sleep(1)
     print("\nHoly mother of go-")
 
-    ##time.sleep(2)
+    time.sleep(2)
 
     print("\nGame over.")
 
@@ -118,7 +118,7 @@ player_rechargeTime = 0
 def fight_choice(attack, health, reload):
     print("** A wild alien appears! **")
 
-    ##time.sleep(2)
+    time.sleep(2)
 
     player_choice = input("Flee or Fight?: ")
     player_choice = player_choice.lower()
@@ -176,7 +176,7 @@ def combat_system(attack, health, reload):
             break
         time.sleep(reload)   
         print("The alien has " + str(alien_hp) + " HP left. **")
-        ##time.sleep(1)
+        time.sleep(1)
         alien_hitchance = random.randint(1, 100)
        
         if alien_hitchance % 2 >= 1 and health > 0:
@@ -271,9 +271,9 @@ def weapon_choice():
     global player_rechargeTime
 
     print("\n** You walk over to the weapon vault and open it. **")
-    ##time.sleep(2)
+    time.sleep(2)
     print("\nThere is a plasma rifle with unlimited ammo, but it takes time to recharge and has lower shooting speed.")
-    ##time.sleep(2)
+    time.sleep(2)
     print("There is also a live ammo assault AR, which does more damage and has a higher firing speed, but there's not a lot of ammo left.\n")
 
     print("Which weapon do you choose?")
@@ -310,19 +310,19 @@ def weapon_choice():
 def control_room_choices():
     while True:
         print("What do you want to do?")
-        ##time.sleep(1.5)
+        time.sleep(1.5)
         print(" 1) Go to the microwave. \n 2) Go to the bridge fridge. \n 3) Go to the star chart. \n 4) Go to the weapon vault. \n (Answer 1, 2, 3, or 4)")
         playerInput=input()
 
         if playerInput=="1":
             print("The lone object in the microwave is a metal spoon.")
-            ##time.sleep(1)
+            time.sleep(1)
             print("Turn it on? (y/n)")
             playerInput=input()
             if playerInput== "y":
-                ##time.sleep(0.5)
+                time.sleep(0.5)
                 print("You turn the microwave on.\n A bright flash is all you see as the ship is rocked by a massive explosion.")
-                ##time.sleep(1)
+                time.sleep(1)
                 print("____    ____  ______    __    __      __        ______        _______. _______ ")
                 print("\   \  /   / /  __  \  |  |  |  |    |  |      /  __  \      /       ||   ____|")
                 print(" \   \/   / |  |  |  | |  |  |  |    |  |     |  |  |  |    |   (----`|  |__   ")
@@ -337,19 +337,19 @@ def control_room_choices():
                 continue
 
         if playerInput=="2":
-            ##time.sleep(1)
+            time.sleep(1)
             print("You look in the fridge. The only contents are some old bottles of space milk.")
-            ##time.sleep(1)
+            time.sleep(1)
             continue
 
         if playerInput=="3":
-            ##time.sleep(1)
+            time.sleep(1)
             print("You walk over to the star chart. It says the ship is currently in the Alpha Centauri star system and about 0.1 light years from Proxima Centauri b.")
-            ##time.sleep(1)
+            time.sleep(1)
             continue
 
         if playerInput=="4":
-            ##time.sleep(1)
+            time.sleep(1)
             weapon_choice()
             break
 
@@ -377,7 +377,7 @@ while True:
 
 import time
 
-##time.sleep(1)
+time.sleep(1)
         
 #-----------------------------------------------------------------------------------------Start of Game-----------------------------------------------------------------------------------------
 
@@ -391,11 +391,11 @@ print(" 1) Walk to your station.\n 2) Walk to the bridge. \n (Answer 1 or 2)")
 playerInput=input()
 
 if playerInput=="1":
-    ##time.sleep(0.5)
+    time.sleep(0.5)
     print("You decide to head to your station, simply putting off the fact that there are no humans to be seen during one of the busiest times of the day.\n")
-    ##time.sleep(1.4)
+    time.sleep(1.4)
     print("***2 minutes later***\n")
-    ##time.sleep(1)
+    time.sleep(1)
     print("As you walk through the engine room's blast door, the sounds of tearing of metal and depressurization alarms rip through the air.  \nYour training tells you that none of these sounds are positive.\n")
 
     print("If you do not make a decision quickly, you have a high likelyhood of being sucked out into space!!")
@@ -406,13 +406,13 @@ if playerInput=="1":
 
     if playerInput=="1":
         print("You shut the door just in time to save yourself. Remembering protocol, you head to the bridge to inform the captain of the hull breach.\n")
-        ##time.sleep(1.4)
+        time.sleep(1.4)
         control_room()
 
     elif playerInput== "2" or "3":
-        ##time.sleep(1)
+        time.sleep(1)
         print("Time seems to slow down as the wall in front of you buckles and tears open. You are sucked into the endless void known as space, never to be seen again")
-        ##time.sleep(1)
+        time.sleep(1)
         print("____    ____  ______    __    __      __        ______        _______. _______ ")
         print("\   \  /   / /  __  \  |  |  |  |    |  |      /  __  \      /       ||   ____|")
         print(" \   \/   / |  |  |  | |  |  |  |    |  |     |  |  |  |    |   (----`|  |__   ")
@@ -425,7 +425,7 @@ if playerInput=="1":
 
 if playerInput=="2":
     print("You walk to the bridge.\n")
-    ##time.sleep(1)
+    time.sleep(1)
     control_room()
 
 console_riddle()
@@ -465,19 +465,19 @@ elif playerInput == "n":
 
 
 
-##time.sleep(1)        
+time.sleep(1)        
 print("You leave the bridge and begin to walk towards the location of the surviving crew.")
-##time.sleep(1)
+time.sleep(1)
 print("Soon you reach an intersection. You peer around the corner to see two aliens!\nThey both seem to have some sort of weapon, however, you cannot tell what it is.")
-##time.sleep(2)
+time.sleep(2)
 print("Do you want to:\n 1) Try to sneak past them.\n 2) Attack them.\n (Answer 1 or 2)")
 playerInput=input()
 playerInput=playerInput.lower()
 
 if playerInput=="1":
-    ##time.sleep(0.5)
+    time.sleep(0.5)
     print("While their backs are turned, you sprint across the intersection.\nIt seems that they didn't notice you.")
-    ##time.sleep(1)
+    time.sleep(1)
     print("However, as you continue down the hallway, you reach a malfunctioning blast door.\nAbove it, is a sign reading *Bridge Crew Cabins*\nThe only way to continue is to fix the door.")
 
 if playerInput=="2":
@@ -493,16 +493,16 @@ while True:
         break
 
 print("As you look at the console, an alert pops up on the screen.")
-##time.sleep(1)
+time.sleep(1)
 print("**Error Section of Software Deleted**")
-##time.sleep(1)
+time.sleep(1)
 print("The only way to get through the door is to fix the software.\nLet's see what the problem is.")
 print("**Fill in the underline(_____) with the correct code.**\n")
-##time.sleep(1)
+time.sleep(1)
 door_code()
 
 print("You slip through the door and into the next hallway.\nYou begin to look into the cabins for any survivors.\nAs you reach the last door, you see a scared face staring out at you.")
-##time.sleep(2)
+time.sleep(2)
 print("Open the door to save the man? (y/n)")
 playerInput==input()
 
@@ -510,7 +510,7 @@ if playerInput=="y":
     print("You access the console to open the door.\n However, you see a similar problem as the previous door that let you have access to the cabin you are currently in.")
     print("You will have to fix the code.")
     print("**Fill in the underline(_____) with the correct code.**\n")
-    ##time.sleep(1)
+    time.sleep(1)
     door_code()
     print("The man thanks you as the door opens.\nYou tell him to get to the escape pods.")
     civs_saved+=1
@@ -519,13 +519,13 @@ else:
     print("You decide to leave the man behind.\n He bangs on the door to get your attention, but you just continue on.")
 
 print("You continue on to the brig.")
-##time.sleep(1.5)
+time.sleep(1.5)
 print("**5 minutes later**")
-##time.sleep(1.5)
+time.sleep(1.5)
 print("You stare down the hallway, in front of the brig's door is an alien.")
-##time.sleep(1)
+time.sleep(1)
 print("There is no way you will be able to sneak past him.")
-##time.sleep(1)
+time.sleep(1)
 print("Your only options are to:\n 1)Run and leave your crewmates behind.\n 2)Fight the alien.\n Answer(1 or 2)")
 
 playerInput=input()
@@ -545,16 +545,16 @@ else:
     print("You all run to the nearest escape pod.")
     print("However as you open the secape pod door, you see that the maximum capacity is 10.")
     print("Do you leave someone behind or stay behind yourself?\n (Remember, JIM stole your orange juice the day before.)")
-    ##time.sleep(1)
+    time.sleep(1)
     print(" 1)Stay behind. 2)Leave JIM behind. Answer(1 or 2)")
     playerInput==input()
 
     if playerInput=="1":
         civs_saved+=10
         print("You decide to stay behind.")
-        ##time.sleep(1)
+        time.sleep(1)
         print("You saved " + str(civs_saved) + "people.")
-        ##time.sleep(1)
+        time.sleep(1)
         print(".___________. __    __       ___      .__   __.  __  ___      _______.    _______   ______   .______         .______    __          ___   ____    ____  __  .__   __.   _______ ")
         print("|           ||  |  |  |     /   \     |  \ |  | |  |/  /     /       |   |   ____| /  __  \  |   _  \        |   _  \  |  |        /   \  \   \  /   / |  | |  \ |  |  /  _____|")
         print("`---|  |----`|  |__|  |    /  ^  \    |   \|  | |  '  /     |   (----`   |  |__   |  |  |  | |  |_)  |       |  |_)  | |  |       /  ^  \  \   \/   /  |  | |   \|  | |  |  __  ")
@@ -567,9 +567,9 @@ else:
         civs_saved+=9
         print("You throw JIM out of the escape pod.\n This is what you get for stealing my orange juice!")
         print("The escape pod ejects from the ship.")
-        ##time.sleep(1)
+        time.sleep(1)
         print("You saved " + str(civs_saved - 1) + "people.")
-        ##time.sleep(1)
+        time.sleep(1)
         print(".___________. __    __       ___      .__   __.  __  ___      _______.    _______   ______   .______         .______    __          ___   ____    ____  __  .__   __.   _______ ")
         print("|           ||  |  |  |     /   \     |  \ |  | |  |/  /     /       |   |   ____| /  __  \  |   _  \        |   _  \  |  |        /   \  \   \  /   / |  | |  \ |  |  /  _____|")
         print("`---|  |----`|  |__|  |    /  ^  \    |   \|  | |  '  /     |   (----`   |  |__   |  |  |  | |  |_)  |       |  |_)  | |  |       /  ^  \  \   \/   /  |  | |   \|  | |  |  __  ")
